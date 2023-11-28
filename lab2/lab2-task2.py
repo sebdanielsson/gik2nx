bac1d4ed428511cc80c6b256065ef44179c0205b
Sebastian <sebastian.danielsson@proton.me>
2023-11-28 12:56:05 +0100

    Linting fixes, fix chart

--- lab2/lab2-task2.py
+++ lab2/lab2-task2.py
@@ -1,6 +1,7 @@
 # Find the value of y
 import numpy as np
 
+
 def calculate_y(a, b):
     a = np.array(a, dtype=np.float64)
     b = np.array(b, dtype=np.float64)
@@ -11,19 +12,21 @@ def calculate_y(a, b):
 
     # Ensure the length of a and b does not exceed n=50 and m=40 respectively
     if len(a) > 50 or len(b) > 40:
-        raise ValueError("Length of array a must not exceed 50 and length of array b must not exceed 40.")
+        raise ValueError(
+            "Length of array a must not exceed 50 and length of array b must not exceed 40."
+        )
 
     # Calculate the first summation
     n = len(a)
     a_padded = np.pad(a, (0, max(0, n - len(a))), constant_values=0)[:n]
     b_padded = np.pad(b, (0, max(0, n - len(b))), constant_values=0)[:n]
-    first_sum = np.sum((a_padded**4 + b_padded**2)**3)
+    first_sum = np.sum((a_padded**4 + b_padded**2) ** 3)
 
     # Calculate the second summation
     m = len(b)
     a_padded_m = np.pad(a, (0, max(0, m - len(a))), constant_values=0)[:m]
     b_padded_m = np.pad(b, (0, max(0, m - len(b))), constant_values=0)[:m]
-    second_sum = np.sum((a_padded_m**2 - b_padded_m)**2)
+    second_sum = np.sum((a_padded_m**2 - b_padded_m) ** 2)
 
     # Return the sum of the two summations
     return first_sum + second_sum
