f67c1f9207438f13c410cb574880dcda918c04c6
Sebastian <sebastian.danielsson@proton.me>
2023-11-30 11:46:01 +0100

    Increase matrix size by 10x for rows and cols

--- lab2/lab2-task3.py
+++ lab2/lab2-task3.py
@@ -1,12 +1,33 @@
 # Find the value of Z
 import numpy as np
 
+matrix_rows = 250
+matrix_cols = 250
+
+
 def calculate_z(A1, A2, B1, B2):
-    # Ensure the dimensions of A1, A2, B1, and B2 are 25x25 and the elements are positive integers less than 100
-    if A1.shape != (25, 25) or A2.shape != (25, 25) or B1.shape != (25, 25) or B2.shape != (25, 25):
-        raise ValueError("Dimensions of A1, A2, B1, and B2 must be 25x25.")
-    if np.any(A1 >= 100) or np.any(A2 >= 100) or np.any(B1 >= 100) or np.any(B2 >= 100) or np.any(A1 <= 0) or np.any(A2 <= 0) or np.any(B1 <= 0) or np.any(B2 <= 0):
-        raise ValueError("Elements in A1, A2, B1, and B2 must be positive integers less than 100.")
+    if (
+        A1.shape != (matrix_rows, matrix_cols)
+        or A2.shape != (matrix_rows, matrix_cols)
+        or B1.shape != (matrix_rows, matrix_cols)
+        or B2.shape != (matrix_rows, matrix_cols)
+    ):
+        raise ValueError(
+            "Dimensions of A1, A2, B1, and B2 must be ${matrix_rows}x${matrix_cols}."
+        )
+    if (
+        np.any(A1 >= 100)
+        or np.any(A2 >= 100)
+        or np.any(B1 >= 100)
+        or np.any(B2 >= 100)
+        or np.any(A1 <= 0)
+        or np.any(A2 <= 0)
+        or np.any(B1 <= 0)
+        or np.any(B2 <= 0)
+    ):
+        raise ValueError(
+            "Elements in A1, A2, B1, and B2 must be positive integers less than 100."
+        )
 
     # Calculate Z1, Z2, and Z
     Z1 = A1 * B1
@@ -17,10 +38,10 @@ def calculate_z(A1, A2, B1, B2):
 
 # Generate 2D matrices for A1, A2, B1, and B2 with integers between 1 and 100
 np.random.seed(1234)  # Set a seed for reproducibility
-A1 = np.random.randint(1, 100, (25, 25))
-A2 = np.random.randint(1, 100, (25, 25))
-B1 = np.random.randint(1, 100, (25, 25))
-B2 = np.random.randint(1, 100, (25, 25))
+A1 = np.random.randint(1, 100, (matrix_rows, matrix_cols))
+A2 = np.random.randint(1, 100, (matrix_rows, matrix_cols))
+B1 = np.random.randint(1, 100, (matrix_rows, matrix_cols))
+B2 = np.random.randint(1, 100, (matrix_rows, matrix_cols))
 
 # Calculate Z
 Z = calculate_z(A1, A2, B1, B2)
